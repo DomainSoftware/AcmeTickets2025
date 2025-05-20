dd6dda8643c010cf030f5722f69c130ad479efbc
Steven Suing <Steven.suing@ais.com>
2025-05-20 08:47:13 -0400

    Moved files around

---
+++ src/Domains/EventManagement/Api/Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers(); // Add support for controllers
+builder.Services.AddOpenApi();
+
+// Add Swagger/OpenAPI services
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+    app.MapOpenApi(); // Exposes the OpenAPI document at /openapi/v1.json
+if (app.Environment.IsDevelopment())
+{
+
+    //http://localhost:5271/swagger/index.html
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
